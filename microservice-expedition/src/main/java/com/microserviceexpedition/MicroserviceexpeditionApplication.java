adfe87ef8a7a392ebc978b68bb4bd52a66d6c9e8
hostaur <aureliehostens@gmail.com>
2019-08-30 14:30:27 -0400

    eureka

--- microservice-expedition/src/main/java/com/microserviceexpedition/MicroserviceexpeditionApplication.java
+++ microservice-expedition/src/main/java/com/microserviceexpedition/MicroserviceexpeditionApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 @EnableDiscoveryClient
 public class MicroserviceexpeditionApplication {
 
